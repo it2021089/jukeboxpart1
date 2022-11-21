3611428e7946077042c8108fd5f1b8237c41c880
it2021089 <118733015+it2021089@users.noreply.github.com>
2022-11-21 22:25:31 +0200

    Update MP3.java

--- jukebox/src/main/java/jukebox_part1/MP3.java
+++ jukebox/src/main/java/jukebox_part1/MP3.java
@@ -7,5 +7,40 @@ import gr.hua.dit.oop2.musicplayer.PlayerException;
 import gr.hua.dit.oop2.musicplayer.PlayerFactory;
 
 public class MP3 {
-	
+	public void playsong(String songFile) 
+	{
+		Player p = PlayerFactory.getPlayer();
+		try {
+		InputStream song = new FileInputStream(songFile);
+		System.out.println("Song is playing...");
+		System.out.println("heynig1");
+		p.play(song);
+		System.out.println("heynig");
+		}catch (FileNotFoundException e) {
+		System.err.println("File "+ songFile+ " not found");
+		} catch (PlayerException e) {
+		System.err.println("Something's wrong with the player: " + e.getMessage());
+		} finally {
+		if (p != null)
+		p.close();
+		}
+	}
+	public void playloop(String songFile) 
+	{
+		while (true) 
+		{
+			Player p = PlayerFactory.getPlayer();
+			try {
+			InputStream song = new FileInputStream(songFile);
+			System.out.println("Song is playing...");
+			System.out.println("heynig1");
+			p.play(song);
+			System.out.println("heynig");
+			}catch (FileNotFoundException e) {
+			System.err.println("File "+ songFile+ " not found");
+			} catch (PlayerException e) {
+			System.err.println("Something's wrong with the player: " + e.getMessage());
+			}
+		}
+	}
 }
